6d3b907cddab3a1fc392ca8a4e93082bf94b8cf5
KyenVu <KyenVu264@gmail.com>
2025-11-13 23:29:22 +0700

    Update Fish.cs

--- Assets/Khanh/Script/Fish/Fish.cs
+++ Assets/Khanh/Script/Fish/Fish.cs
@@ -1,8 +1,24 @@
+using System.Collections;
 using UnityEngine;
 
 public class Fish : BaseUnit
 {
     private SpriteRenderer _spriteRenderer;
+    private bool isCaught = false;
+    private bool facingRight = false;
+    private bool facingLeft = false;
+
+    private Coroutine fightCoroutine;
+
+    private void OnEnable()
+    {
+        CastLineControl.OnFishCaught += GotCaught;
+    }
+    private void OnDisable()
+    {
+        CastLineControl.OnFishCaught -= GotCaught;
+    }
+
 
     public void Initialize()
     {
@@ -32,4 +48,53 @@ public class Fish : BaseUnit
             Debug.Log($"{name} price: {enemySO.price} | rarity: {enemySO.rarity}");
         }
     }
+    private void GotCaught()
+    {
+        isCaught = true;
+        fightCoroutine = StartCoroutine(FightPattern());
+
+    }
+
+    private IEnumerator FightPattern()
+    {
+        while (isCaught && unitData.health > 0)
+        {
+            int action = Random.Range(0, 3);
+
+            switch (action)
+            {
+                case 0:
+                    OnTurnRight();
+                    break;
+                case 1:
+                    OnTurnLeft();
+                    break;
+                case 2:
+                    OnAttack();
+                    break;
+            }
+            float waitTime = Random.Range(1f, 3f);
+            yield return new WaitForSeconds(waitTime);
+        }
+    }
+
+    // The fish will turn right and only take damage when player attacks right, attack player when player attacks left
+    private void OnTurnRight()
+    {
+        if(!isCaught) return;
+        Debug.Log("Fish Attack Right!");
+    }
+    // The fish will turn left and only take damage when player attacks left, attack player when player attacks right
+    private void OnTurnLeft()
+    {
+        if(!isCaught) return;
+        Debug.Log("Fish Attack Left!");
+    }
+    // The fish will attack the player and player have to parry to avoid damage
+    private void OnAttack()
+    {
+        if(!isCaught) return;
+        Debug.Log("Fish Parry!");
+    }
+
 }
