aeaa8d3c596c4cee3bb6badc55a03b98f6a61eab
Davetheduck03 <156407317+Davetheduck03@users.noreply.github.com>
2025-12-01 17:16:10 +0700

    Update EquippableData.cs

--- Assets/Khanh/Script/Inventory/EquippableData.cs
+++ Assets/Khanh/Script/Inventory/EquippableData.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "EquippableData", menuName = "FishyWishy/ItemData/EquippableData")]
 public class EquippableData : ItemData
 {
-    
+    public string runtimeUID;
+
     public virtual void OnEquip()
     {
 
